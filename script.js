f54623dd12e39537251ebfedd258e1430000ba6d
Gokulnath <150780192+Gokul461@users.noreply.github.com>
2024-12-14 13:20:30 +0530

    script.js

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+  // function changeImage() {
+  //   var image = document.getElementById('profile-pic');
+  //   if (image.src.includes('me6.jpg')) {
+  //     image.src = 'assets/aboutme2.jpg'; // New image
+  //   } else {
+  //     image.src = 'assets/me6.jpg'; // Original image
+  //   }
+  // }
+  
